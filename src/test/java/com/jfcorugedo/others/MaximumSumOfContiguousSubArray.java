ae8ab8b5b0a0dae4cf90b8bb08451672c3a763da
jfcorugedo <jfcorugedo@gmail.com>
2021-02-04 08:22:55 +0100

    chore: adds another solution to MaximumSumOfContiguousSubArray, now with just one loop

--- src/test/java/com/jfcorugedo/others/MaximumSumOfContiguousSubArray.java
+++ src/test/java/com/jfcorugedo/others/MaximumSumOfContiguousSubArray.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
+import static java.lang.Math.max;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -57,5 +58,39 @@ public class MaximumSumOfContiguousSubArray {
 
         System.out.println(String.format("maximum sub array: %s", Arrays.toString(Arrays.copyOfRange(x, from-1, to))));
         return maxValue;
+    }
+
+    @Test
+    public void maximumSumOfContiguousSubArray2() {
+
+        assertThat(solution2(new int[0])).isZero();
+        assertThat(solution2(new int[]{1})).isEqualTo(1);
+        assertThat(solution2(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4})).isEqualTo(6); //[4, -1, 2, 1]
+        assertThat(solution2(new int[]{-2, 1, -3, 4, -1, 2, 1, -1, 4, 3, -1, 3, -2, 5})).isEqualTo(17); //[4, -1, 2, 1, -1, 4, 3, -1, 3, -2, 5]
+    }
+
+    private int solution2(int[] x) {
+        if(x.length == 0) return 0;
+
+        /*
+        Now instead of using two dimensions, I'm going to use just one dimension.
+        So R[i] will represents the maximum sum of a sub array ending in 'i' element.
+        So R[6] represents the maximum sum of a sub array that ends at 6th element.
+
+        Let's decompose the problem:
+
+        Given that R[i] = X(j) + X(j+1) + X(j+2) + ... + A(i-1) + A(i) where j <= i
+        And that R[i-1] = X(j) + X(j+1) + X(j+2) + ... + A(i-1)
+        Then R[i] = max{ X(i) , R[i-1] + X(i) }
+
+        The solution will be the maximum of the numbers inside R
+         */
+        int R[] = new int[x.length + 1];
+
+        for(int i = 1 ; i <= x.length ; i++) {
+                R[i] = max(R[i-1] + x[i-1], x[i-1]);//Continue the last sub arrray or start a new one depending on the value
+        }
+
+        return Arrays.stream(R).max().getAsInt();
     }
 }
