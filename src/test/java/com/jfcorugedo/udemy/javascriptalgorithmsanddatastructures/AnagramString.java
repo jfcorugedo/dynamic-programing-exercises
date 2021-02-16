da76cfa2f24df3691712f4c5e597587d7475f283
jfcorugedo <jfcorugedo@gmail.com>
2021-02-16 19:00:46 +0100

    feat: adds AnagramString implementation using frequency counters

--- src/test/java/com/jfcorugedo/udemy/javascriptalgorithmsanddatastructures/AnagramString.java
+++ src/test/java/com/jfcorugedo/udemy/javascriptalgorithmsanddatastructures/AnagramString.java
@@ -3,6 +3,8 @@ package com.jfcorugedo.udemy.javascriptalgorithmsanddatastructures;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -66,6 +68,42 @@ public class AnagramString {
         boolean same = true;
         for (int i = 0; i < firstSorted.length && same ; i++) {
             same = firstSorted[i] == secondSorted[i];
+        }
+
+        return same;
+    }
+
+    @Test
+    public void notAnagram2() {
+
+        assertThat(solution2("cinema", "icemam")).isFalse();
+    }
+
+    @Test
+    public void anagramString2() {
+
+        assertThat(solution2("cinema", "iceman")).isTrue();
+    }
+
+    /**
+     * Same problem but now using frequency counter pattern
+     */
+    private boolean solution2(String first, String second) {
+        if(first == second) return true;//true only if both are null
+        if(first.equals(second)) return true;
+        if(first.length() != second.length()) return false;
+
+        Map<Character, Integer> firstCounter = new HashMap<>();
+        Map<Character, Integer> secondCounter = new HashMap<>();
+
+        for (int i = 0; i < first.length(); i++) {
+            firstCounter.put(first.charAt(i), firstCounter.computeIfAbsent(first.charAt(i), k -> 0) + 1);
+            secondCounter.put(second.charAt(i), secondCounter.computeIfAbsent(second.charAt(i), k -> 0) + 1);
+        }
+
+        boolean same = true;
+        for (int i = 0; i < first.length() && same ; i++) {
+            same = firstCounter.get(first.charAt(i)).equals(secondCounter.get(first.charAt(i)));
         }
 
         return same;
