9450e6a28acc11832ff2a0bdae5325f1c196caa2
jfcorugedo <jfcorugedo@gmail.com>
2021-02-26 12:44:23 +0100

    chore: improve docs and code

--- src/test/java/com/jfcorugedo/udemy/dynamicprogramming2/CuttingRods.java
+++ src/test/java/com/jfcorugedo/udemy/dynamicprogramming2/CuttingRods.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.lang.Math.max;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -28,17 +29,19 @@ public class CuttingRods {
      * max(p1 + r(n-1), p2 + r(n-2), ...., p(n-1) + r1, p(n))
      *
      * i.e:
-     * R[4] = max{p1 + R[3], p2 + R[2], p3 + R[1], p4 + R[0]}
+     * r(4) = max{p1 + r(3), p2 + r(2), p3 + r(1), p4 + r(0)}
      *
-     * This solution has exponential complexity O(n^2)
+     * Since we are using recursion, this solution has exponential complexity O(n^2)
      */
     @Test
     public void cuttingRods() {
 
+        assertThat(revenue(0, Arrays.asList(1,5))).isZero();
         assertThat(revenue(5, Arrays.asList(1,5,8,9,10))).isEqualTo(13);
         assertThat(revenue(10, Arrays.asList(1,5,8,9,10,17,17,20,24,30))).isEqualTo(30);
         assertThat(revenue(30, IntStream.range(1,31).mapToObj(i -> i).collect(Collectors.toList()))).isEqualTo(30);
     }
+
 
     private int revenue(int length, List<Integer> prices) {
         if(length == 0) return 0; //A rod with no length does not have any value
@@ -68,26 +71,34 @@ public class CuttingRods {
     @Test
     public void cuttingRods2() {
 
+        assertThat(revenue2(0, Arrays.asList(1,5))).isZero();
         assertThat(revenue2(5, Arrays.asList(1,5,8,9,10))).isEqualTo(13);
         assertThat(revenue2(10, Arrays.asList(1,5,8,9,10,17,17,20,24,30))).isEqualTo(30);
         assertThat(revenue2(30, IntStream.range(1,31).mapToObj(i -> i).collect(Collectors.toList()))).isEqualTo(30);
     }
 
+    /**
+     * Instead of using recursion, we can compute each value and store it in an array.
+     * So now we can calculate the max revenue of each lenght using previous calculated values
+     *
+     * R[n] = max{P(1) + R[n-1], P(2) + R[n-2], ..., P(n) + R[0]}
+     * R[0] = 0
+     * R[1] = P(1) + R[0]
+     * R[2] = max{ P(1) + R[1], P(2) + R[0] }
+     * R[3] = max{ P(1) + R[2], P(2) + R[1], P(3) + R[0] }
+     * ...
+     *
+     */
     private int revenue2(int length, List<Integer> prices) {
-        //R[n] = max{P(1) + R[n-1], P(2) + R[n-2], ..., P(n) + R[0]}
-        //R[0] = 0
-        //R[1] = P(1) + R[0]
-        //R[2] = max{ P(1) + R[1], P(2) + R[0] }
-        //R[3] = max{ P(1) + R[2], P(2) + R[1], P(3) + R[0] }
-        //...
+
         int[] R = new int[length+1];
         R[0] = 0;
+        R[1] = prices.get(0);
 
-        for(int i = 1 ; i <= length ; i++) {
-            int maxValue = -1;
-            for(int j = 1 ; j <= i; j++) {
-                int value = prices.get(j-1) + R[i-j];
-                if(value > maxValue) maxValue = value;
+        for(int i = 2 ; i <= length ; i++) {
+            int maxValue = prices.get(i-1);
+            for(int j = 1 ; j < i; j++) {
+                maxValue = max(maxValue, prices.get(j-1) + R[i-j]);
             }
             R[i] = maxValue;
         }
